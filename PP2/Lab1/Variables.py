2c362d944223cd7eb02f423ee4d9ce10202281c5
DanialSagyndyk <157244549+DanialSagyndyk@users.noreply.github.com>
2024-01-24 11:26:55 +0600

    Add files via upload

---
+++ PP2/Lab1/Variables.py
@@ -0,0 +1,25 @@
+#ex1
+carname = "Volvo"
+
+#Ex 2
+x = 50
+
+#Ex 3
+x = 5
+y = 10
+print (x+y)
+
+#Ex 4
+x = 5
+y = 10
+z = 5 + 10
+print (z)
+
+#Ex 5
+x,y,z = "Orange", "Banana", "Cherry"
+
+#Ex 6
+x = y = z = "Orange"
+
+#Ex 7
+
---
+++ PP2/Lab1/syntax.py
@@ -0,0 +1,5 @@
+#EX1
+print("Hello World")
+#Ex2
+if 5>2:
+    print("YES")
